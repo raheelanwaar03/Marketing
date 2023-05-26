083f369f0319540c9104d54300f9c85dd2c6ec41
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-05-26 15:46:13 +0500

    new changesx

--- resources/views/landingPage/store/index.blade.php
+++ resources/views/landingPage/store/index.blade.php
@@ -21,138 +21,47 @@
     <div class="section">
         <div class="container">
             <div class="row">
-                <div class="content col-md-12">
-                    <div class="post-wrapper single-store">
-                        @forelse ($stores as $store)
-                            <div class="row">
-                                <div class="col-md-4 col-sm-4 col-xs-12">
-                                    <div class="post-media text-center">
-                                        <a href="#"><img src="{{ asset('images/' . $store->store_img) }}"
-                                                alt="" class="img-responsive"></a>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-sm-8 col-xs-12">
-                                    <div class="coupon-meta">
-                                        <h3>{{ $store->store_name }}</h3>
-                                        <p>{{ $store->store_des }}</p>
-                                    </div>
-                                    <div class="coupon-bottom clearfix">
-                                        <div class="post-sharing">
-                                            <ul class="list-inline">
-                                                <li><a href="{{ route('LandingPage.Single.Store', ['store_slug' => $store->store_slug, 'id' => $store->id]) }}"
-                                                        class="fb-button btn btn-primary"><span class="hidden-xs">See All
-                                                            Coupons on this store</span></a></li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @empty
-                            <h3>NO store avaliable</h3>
-                        @endforelse
-                    </div>
-                    <hr class="invis3">
-                    <div class="">
-                    </div>
-                </div>
+                @forelse ($stores as $store)
+                    <a href="{{ route('LandingPage.Single.Store', ['store_slug' => $store->store_slug]) }}">
+                        <div class="col-md-2">
+                            <img src="{{ asset('images/' . $store->store_img) }}" class="img-responsive" height="100px"
+                                width="100px" alt="image">
+                        </div>
+                    </a>
+                @empty
+                    <h4>No Store</h4>
+                @endforelse
             </div>
+        </div>
+        <div class="container">
+
             <div class="footer-content">
                 <div class="row">
                     <div class="col-md-12">
-                        <h4>Popular Categories</h4>
+                        <h4>All Stores</h4>
                     </div>
                 </div>
                 <div class="footer-content">
                     <div class="row">
                         <div class="col-md-12">
-                            <h4>Popular Categories</h4>
+                            <h4>Store Names</h4>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-2 col-sm-2 col-xs-12">
-                            <ul class="customlist">
-                                <li><a href="#">Baby Kids</a></li>
-                                <li><a href="#">Books & Magazines</a></li>
-                                <li><a href="#">Computers</a></li>
-                                <li><a href="#">Cameras</a></li>
-                                <li><a href="#">Electronics</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="col-md-2 col-sm-2 col-xs-12">
-                            <ul class="customlist">
-                                <li><a href="#">Games</a></li>
-                                <li><a href="#">Gifts</a></li>
-                                <li><a href="#">Health Beauty</a></li>
-                                <li><a href="#">Home Garden</a></li>
-                                <li><a href="#">Home Supplies</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="col-md-2 col-sm-2 col-xs-12">
-                            <ul class="customlist">
-                                <li><a href="#">Laptops</a></li>
-                                <li><a href="#">Entertainetmen</a></li>
-                                <li><a href="#">Digital Stores</a></li>
-                                <li><a href="#">Marketplaces</a></li>
-                                <li><a href="#">Musicians</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="col-md-2 col-sm-2 col-xs-12">
-                            <ul class="customlist">
-                                <li><a href="#">Movies & Films</a></li>
-                                <li><a href="#">Phones</a></li>
-                                <li><a href="#">Travel</a></li>
-                                <li><a href="#">Televisions</a></li>
-                                <li><a href="#">Telegraphers</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="col-md-2 col-sm-2 col-xs-12">
-                            <ul class="customlist">
-                                <li><a href="#">Baby Toys</a></li>
-                                <li><a href="#">Clothings</a></li>
-                                <li><a href="#">Jewellry</a></li>
-                                <li><a href="#">Car Supplies</a></li>
-                                <li><a href="#">Watches</a></li>
-                            </ul>
-                        </div>
-
-                        <div class="col-md-2 col-sm-2 col-xs-12">
-                            <ul class="customlist">
-                                <li><a href="#">Glassess</a></li>
-                                <li><a href="#">Medical</a></li>
-                                <li><a href="#">Pet Shops</a></li>
-                                <li><a href="#">LifeStyle</a></li>
-                                <li><a href="#">Sports</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <!-- end row -->
-
-                    <hr class="">
-
-                    <div class="row">
-                        <div class="col-md-12">
-                            <h4>Popular Stores</h4>
-                        </div>
-                        <!-- end col -->
-                    </div>
-
-                    <div class="text-center store-list row">
                         @forelse ($stores as $store)
-                            <div class="col-md-2 col-sm-4 col-xs-6">
-                                <div class="post-media">
-                                    <a href="#"><img src="{{ asset('images/'.$store->store_img) }}" alt="Store img"
-                                            class="img-responsive"></a>
-                                    <small>{{ $store->store_name }}</small>
-                                </div>
+                            <div class="col-md-2 col-sm-2 col-xs-12">
+                                <ul class="customlist">
+                                    <li><a
+                                            href="{{ route('LandingPage.Single.Store', ['store_slug' => $store->store_slug]) }}">{{ $store->store_name }}</a>
+                                    </li>
+                                </ul>
                             </div>
                         @empty
-                            <h3>No store available</h3>
+                            <h4>No Store Avaliable</h4>
                         @endforelse
                     </div>
+
+                    <hr class="">
                 </div>
             </div>
         </div>
