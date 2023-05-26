0940978db287548551384e24365a8d512a3fce90
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-05-26 23:32:16 +0500

    changes on landing page

--- resources/views/landingPage/welcome.blade.php
+++ resources/views/landingPage/welcome.blade.php
@@ -325,65 +325,15 @@
                 </div>
 
                 <div class="row">
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <ul class="customlist">
-                            <li><a href="#">Baby Kids</a></li>
-                            <li><a href="#">Books & Magazines</a></li>
-                            <li><a href="#">Computers</a></li>
-                            <li><a href="#">Cameras</a></li>
-                            <li><a href="#">Electronics</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <ul class="customlist">
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Gifts</a></li>
-                            <li><a href="#">Health Beauty</a></li>
-                            <li><a href="#">Home Garden</a></li>
-                            <li><a href="#">Home Supplies</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <ul class="customlist">
-                            <li><a href="#">Laptops</a></li>
-                            <li><a href="#">Entertainetmen</a></li>
-                            <li><a href="#">Digital Stores</a></li>
-                            <li><a href="#">Marketplaces</a></li>
-                            <li><a href="#">Musicians</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <ul class="customlist">
-                            <li><a href="#">Movies & Films</a></li>
-                            <li><a href="#">Phones</a></li>
-                            <li><a href="#">Travel</a></li>
-                            <li><a href="#">Televisions</a></li>
-                            <li><a href="#">Telegraphers</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <ul class="customlist">
-                            <li><a href="#">Baby Toys</a></li>
-                            <li><a href="#">Clothings</a></li>
-                            <li><a href="#">Jewellry</a></li>
-                            <li><a href="#">Car Supplies</a></li>
-                            <li><a href="#">Watches</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="col-md-2 col-sm-2 col-xs-12">
-                        <ul class="customlist">
-                            <li><a href="#">Glassess</a></li>
-                            <li><a href="#">Medical</a></li>
-                            <li><a href="#">Pet Shops</a></li>
-                            <li><a href="#">LifeStyle</a></li>
-                            <li><a href="#">Sports</a></li>
-                        </ul>
-                    </div>
+                    @foreach ($categorys as $category)
+                        <div class="col-md-2 col-sm-2 col-xs-12">
+                            <ul class="customlist">
+                                <li><a
+                                        href="{{ route('LandingPage.Single.Category', ['category_slug' => $categroy->category_slug]) }}">{{ $category->category_name }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    @endforeach
                 </div><!-- end row -->
             </div><!-- end ttmenu-content -->
         </div>
