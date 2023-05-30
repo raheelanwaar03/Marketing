7e7133f61b1a80893907a716839bba696451a535
raheelanwaar03 <raheelanwaar0316@gmail.com>
2023-05-30 13:48:10 +0500

    h and w

--- resources/views/landingPage/welcome.blade.php
+++ resources/views/landingPage/welcome.blade.php
@@ -90,8 +90,8 @@
             <div class="text-center store-list row">
                 @forelse ($stores as $store)
                     <div class="col-md-2 col-sm-4 col-xs-6 wow fadeInUp" data-wow-duration="1s" data-wow-delay="0.2s">
-                        <div class="post-media">
-                            <a href="#" style="height:80px;width:80px;"><img
+                        <div class="post-media" style="height:80px;width:80px;">
+                            <a href="#"><img
                                     src="{{ asset('images/' . $store->store_img) }}" class="img-responsive"></a>
                             <small>{{ $store->store_name }}</small>
                         </div>
